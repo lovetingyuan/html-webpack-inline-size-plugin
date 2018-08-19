86f3b0c425527fd1aa6d06cf75e7c7ff19b511e9
Baitu Huang <baitu.huang@shopeemobile.com>
2018-08-20 01:07:34 +0800

    support webpack v3 and v4

--- src/index.js
+++ src/index.js
@@ -1,36 +1,47 @@
 import assert from 'assert';
 import path from 'path';
 import HtmlWebpackInlineSourcePlugin from 'html-webpack-inline-source-plugin';
+const pluginName = 'HtmlWebpackInlineSizePlugin';
 
 export default class HtmlWebpackInlineSizePlugin {
   constructor(options) {
-    assert.equal(options, undefined, 'The HtmlWebpackInlineSizePlugin does not accept any options');
+    assert.equal(options, undefined, `${pluginName} does not accept any options`);
+  }
+  _filterFiles(compilation, htmlPluginData, callback) {
+    const inlineSize = Number(htmlPluginData.plugin.options.inlineSize);
+    const inlineSource = htmlPluginData.plugin.options.inlineSource;
+    if (!inlineSize) { return callback(); }
+    const chunkFiles = [
+      ...htmlPluginData.assets.css,
+      ...htmlPluginData.assets.js
+    ].map(fileName => {
+      if (!/\.(js|css)$/.test(fileName)) return;
+      const name = path.posix.relative(htmlPluginData.assets.publicPath, fileName);
+      const asset = compilation.assets[name];
+      if (asset && typeof asset.size === 'function' && asset.size() <= inlineSize) {
+        return `(${name})`;
+      }
+    }).concat(inlineSource ? `(${inlineSource})` : '').filter(Boolean);
+    if (!chunkFiles.length) return callback();
+    htmlPluginData.plugin.options.inlineSource = chunkFiles.join('|');
+    callback(null, htmlPluginData);
   }
   apply(compiler) {
-    compiler.apply(new HtmlWebpackInlineSourcePlugin());
-    compiler.plugin('compilation', function(compilation) {
-      compilation.plugin('html-webpack-plugin-before-html-generation', function(htmlPluginData, callback) {
-        const inlineSize = Number(htmlPluginData.plugin.options.inlineSize);
-        const inlineSource = htmlPluginData.plugin.options.inlineSource;
-        if (!inlineSize) { return callback(); }
-        const cssAndJsList = [
-          ...htmlPluginData.assets.css,
-          ...htmlPluginData.assets.js
-        ];
-        const chunkFiles = cssAndJsList.map(fileName => {
-          const name = path.posix.relative(htmlPluginData.assets.publicPath, fileName);
-          if (/\.(js|css)$/.test(name)) {
-            const asset = compilation.assets[name];
-            if (asset && typeof asset.size === 'function' && asset.size() <= inlineSize) {
-              return `(${name})`;
-            }
-          }
-        }).concat(inlineSource ? `(${inlineSource})` : '').filter(Boolean);
-        if (chunkFiles.length) {
-          htmlPluginData.plugin.options.inlineSource = chunkFiles.join('|');
-        }
-        callback(null, htmlPluginData);
+    const inlineSourcePlugin = new HtmlWebpackInlineSourcePlugin();
+    if (compiler.hooks) {
+      compiler.hooks.compilation.tap(pluginName, (compilation) => {
+        compilation.hooks.htmlWebpackPluginBeforeHtmlGeneration.tapAsync(pluginName, (...args) => {
+          this._filterFiles(compilation, ...args);
+        });
       });
-    });
+      inlineSourcePlugin.apply(compiler);
+    } else {
+      compiler.plugin('compilation', function(compilation) {
+        compilation.plugin('html-webpack-plugin-before-html-generation', (...args) => {
+          this._filterFiles(compilation, ...args);
+        });
+      });
+      compiler.apply.apply(compiler, [inlineSourcePlugin]);
+    }
   }
 }
